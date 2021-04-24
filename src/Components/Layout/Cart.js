06c8de7f105f213cfc10c44b619b3c3f1cb1dede
Cristóbal Torres <cristobal.torres@rank.com>
2021-04-24 17:28:48 -0500

    Minor stylings fixes for cart

--- src/Components/Layout/Cart.js
+++ src/Components/Layout/Cart.js
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import CartItem from './CartItem';
 import CartContext from '../../store/cart-context';
+import styles from './Cart.module.scss';
 
 import Modal from '../UI/Modal';
 
@@ -11,14 +12,14 @@ const Cart = props => {
   const cartItems = cartCtx.items.map(item => <CartItem key={item.id} price={item.price} name={item.name} amount={item.amount} onRemove={cartItemRemoveHandler.bind(null, item.id)} onAdd={cartItemAddHandler.bind(null, item)} />);
   const totalAmount = `$ ${cartCtx.totalAmount.toFixed(2)}`;
   return (
-    <Modal toggleModal={props.toggleModal}>
-      <ul>
+    <Modal  toggleModal={props.toggleModal}>
+      <ul className={styles['cart__list']}>
         {cartItems}
-        <div>
-          <span>Total Amount</span>
+        <div className={styles['cart__total']}>
+          <span>Total Amount: </span>
           <span>{totalAmount}</span>
         </div>
-        <div>
+        <div className={styles['cart__actions']}>
           <button onClick={props.toggleModal}>Close</button>
           {cartCtx.items.length > 0 && <button>Order</button>}
         </div>
--- src/Components/Layout/CartItem.js
+++ src/Components/Layout/CartItem.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import styles from './CartItem.module.scss';
+import React from "react";
+import styles from "./CartItem.module.scss";
 
-const CartItem = (props) =>{
-  const price = `$ ${props.price.toFixed(2)}`;
+const CartItem = (props) => {
+	const price = `$ ${props.price.toFixed(2)}`;
 
-  return(
-    <li className='cart__item'>
-      <div>
-        <h2>{props.name}</h2>
-        <div className={styles['cart__item__summary']}>
-          <span className={styles['cart__item__summary-amount']}>{props.amount}</span>
-          <span>&times;</span>
-          <span className={styles['cart__item__summary-price']}>{price}</span>
-          <span> = </span>
-          <span>{`$ ${props.price * props.amount}`}</span>   
-          
-        </div>
-        <div className={styles['cart__item__actions']}>
-          <button onClick={props.onRemove}>-</button>
-          <button onClick={props.onAdd}>+</button>
-        </div>
-      </div>
-    </li>
-  )
-}
+	return (
+		<li className={styles['cart__item']}>
+			<div>
+				<h2>{props.name}</h2>
+				<div className={styles["cart__item__summary"]}>
+					<span className={styles["cart__item__summary-amount"]}>{props.amount}</span>
+					<span style={{ marginLeft: "1rem", marginRight: "1rem" }}>&times;</span>
+					<span className={styles["cart__item__summary-price"]}>{price}</span>
+					<span> = </span>
+					<span>{`$ ${(props.price * props.amount).toFixed(2)}`}</span>
+				</div>
+				<div className={styles["cart__item__actions"]}>
+					<button onClick={props.onRemove}>-</button>
+					<button onClick={props.onAdd}>+</button>
+				</div>
+			</div>
+		</li>
+	);
+};
 
 export default CartItem;
