aa95ab649e7c84638741cb557a2f43b9ede59be2
Cristobal_Torres <cristobalt@spacebarmedia.com>
2021-04-27 15:11:25 -0500

    FIX - Footer portal

--- src/Components/Layout/Footer.js
+++ src/Components/Layout/Footer.js
@@ -1,13 +1,26 @@
+import { Fragment } from 'react';
+import ReactDom from 'react-dom';
 import styles from './Footer.module.scss';
+const portalElement = document.getElementById('footer');
+
+const FooterWrapper = ()=>{
+  return(
+    <span className={styles['footer']}>
+    <p>
+      Written with &lt;3 by <a href="https://gitlab.com/delebrindel" target="git">delebrindel</a>
+    </p>
+    <p>
+      Github repo <a href="https://github.com/delebrindel/macanchis" target="git">here</a>
+    </p>
+    </span>
+  )
+}
 
 const Footer = (props) => {
   return (
-    <div className={styles['footer']}>
-      <p>
-        Written with &lt;3 by <a href="https://gitlab.com/delebrindel" target="git">delebrindel</a>
-      </p>
-      Github repo <a href="https://github.com/delebrindel/macanchis" target="git">here</a>
-    </div>
+    <Fragment>
+      {ReactDom.createPortal(<FooterWrapper />, portalElement)}
+    </Fragment>
   )
 }
 
