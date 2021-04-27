fd2dfbc011a25db94ad435079ed669d148d000f6
Cristobal_Torres <cristobalt@spacebarmedia.com>
2021-04-27 14:41:13 -0500

    FIX - Cart logic and 'order' method

--- src/Components/Layout/Cart.js
+++ src/Components/Layout/Cart.js
@@ -7,23 +7,40 @@ import Modal from '../UI/Modal';
 
 const Cart = props => {
   const cartCtx = useContext(CartContext);
-  const cartItemRemoveHandler = (id) => { };
-  const cartItemAddHandler = (item) => { };
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+   };
+  const cartItemAddHandler = (item ) => { 
+    cartCtx.addItem({...item, amount: 1});
+  };
+
+  const orderHandler = ()=>{
+    console.log("=======================");
+    console.log("Order is being handled!");
+    console.log("=======================");
+    cartCtx.items.forEach((item)=>{
+      console.log(`Ordering ${item.amount} x ${item.name}, $ ${(item.amount * item.price).toFixed(2)}`);
+    })
+    console.log("=======================");
+    console.log(`Your total is $ ${cartCtx.totalAmount.toFixed(2)}`)
+    console.log("=======================");
+  }
+
   const cartItems = cartCtx.items.map(item => <CartItem key={item.id} price={item.price} name={item.name} amount={item.amount} onRemove={cartItemRemoveHandler.bind(null, item.id)} onAdd={cartItemAddHandler.bind(null, item)} />);
   const totalAmount = `$ ${cartCtx.totalAmount.toFixed(2)}`;
   return (
     <Modal  toggleModal={props.toggleModal}>
       <ul className={styles['cart__list']}>
         {cartItems}
+      </ul>
         <div className={styles['cart__total']}>
           <span>Total Amount: </span>
           <span>{totalAmount}</span>
         </div>
         <div className={styles['cart__actions']}>
           <button onClick={props.toggleModal}>Close</button>
-          {cartCtx.items.length > 0 && <button>Order</button>}
+          {cartCtx.items.length > 0 && <button onClick={orderHandler}>Order</button>}
         </div>
-      </ul>
     </Modal>
   )
 }
--- src/Components/Layout/CartButton.js
+++ src/Components/Layout/CartButton.js
@@ -1,14 +1,29 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 import styles from './CartButton.module.scss';
 import CartIcon from './CartIcon';
 
 const CartButton = (props)=>{
+  const [buttonHighlighted, setButtonHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
   const numberOfItems = cartCtx.items.reduce((accumulator, item)=>{ return accumulator + item.amount }, 0)
+  const buttonClasses = `${styles['navbar__cart']} ${buttonHighlighted ? styles['navbar__cart--bump'] : ''}`;
+  const {items } = cartCtx;
+
+  useEffect(()=>{
+    if(items.length > 0){
+      setButtonHighlighted(true);
+      const timer = setTimeout(()=>{ setButtonHighlighted(false)}, 900);
+
+      return ()=>{
+        clearTimeout(timer);
+      }
+    } 
+
+  }, [items])
 
   return (
-    <button className={styles['navbar__cart']} onClick={props.toggleModal}>
+    <button className={buttonClasses} onClick={props.toggleModal}>
       <span className={styles['navbar__cart__icon']}><CartIcon /></span>
       <span className={styles['navbar__cart__badge']}>{numberOfItems}</span>
     </button>
